e5be4bcda83603d54034e230e85a13a32470abe8
I336718 <rutger_rauws@hotmail.com>
2015-11-25 16:53:04 +0100

    Added all methods of the interfaces to UnoService.cs

--- UNO/UNOService/UnoService.cs
+++ UNO/UNOService/UnoService.cs
@@ -8,16 +8,82 @@ using System.Text;
 namespace UNOService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class UnoService
+    public class UnoService : ILobby, ILogin, ISignUp, IGame
     {
-    //    public void Login(string userName, string password)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+		bool ILogin.Login(string userName, string password)
+		{
+			throw new NotImplementedException();
+		}
 
-    //    bool ILogin.Login(string userName, string password)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+		bool ISignUp.SignUp(string userName, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		String ISignUp.CheckUserName(String userName)
+		{
+			throw new NotImplementedException();
+		}
+
+		List<Player> ILobby.GetOnlineList()
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.CreateParty()
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.LeaveParty()
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.SendInvites(List<Player> players)
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.AnswerInvite(bool answer)
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.StartGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		void ILobby.SendMessageLobby(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		
+		void IGame.SaveReplay(int gameID)
+		{
+			throw new NotImplementedException();
+		}
+
+		Card IGame.takeCard()
+		{
+			throw new NotImplementedException();
+		}
+
+		void IGame.playCard(Card c)
+		{
+			throw new NotImplementedException();
+		}
+
+		void IGame.LeaveGame()
+		{
+			throw new NotImplementedException();
+		}
+
+		void IGame.SendMessageGame(string message)
+		{
+			throw new NotImplementedException();
+		}
     }
 }
