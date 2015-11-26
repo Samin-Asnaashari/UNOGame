3e8f3ae1459fa0646b7349591747ca36fb3a36b8
merv0190 <mervin.v01@hotmail.com>
2015-11-26 21:26:04 +0100

    Added queries

--- UNO/UNOService/DatabaseHandler.cs
+++ UNO/UNOService/DatabaseHandler.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace UNOService
 {
     public class DatabaseHandler
     {
+        private MySqlConnection connection;
+        public DatabaseHandler()
+        {
+            string connectionInfo = "server = 127.0.0.1;" + "database = uno;" + "user id = root;" + "password = ;" + "connect timeout = 30;";
+            connection = new MySqlConnection(connectionInfo);
+        }
         public void InsertPlayerWon(string username)
         {
 
@@ -18,9 +25,25 @@ namespace UNOService
 
         }
 
-        public void InsertPlayer()
+        public void InsertPlayer(String username, String password)
         {
-
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd;
+                String sql = "INSERT INTO `players` (`Username`, `Password`, `GamesWon`, `GamesPlayed`) VALUES ('" + username + "', '" + password + "', NULL, NULL);";
+                cmd = new MySqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         public void InsertMove()
@@ -35,7 +58,27 @@ namespace UNOService
 
         public bool CheckUserName(string username)
         {
-            return false;
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd;
+                String sql = "SELECT count(*) FROM `players` where Username = '" + username + "'";
+                cmd = new MySqlCommand(sql, connection);
+                int numberRows = Convert.ToInt32(cmd.ExecuteScalar());
+                connection.Close();
+                if (numberRows == 1)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
