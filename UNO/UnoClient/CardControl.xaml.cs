4e1b5e21789b5e12944348e08937e04b094bcdd8
Samin-Asnaashari <asn.samin@gmail.com>
2015-11-27 16:56:22 +0100

    Game window GUI

---
+++ UNO/UnoClient/CardControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnoClient
+{
+    /// <summary>
+    /// Interaction logic for CardControl.xaml
+    /// </summary>
+    public partial class CardControl : UserControl
+    {
+        proxy.Card Card;
+        public CardControl()
+        {
+            InitializeComponent();
+            Card = new proxy.Card();
+        }
+
+    }
+}
