19796a01565df211238ed72eed00f8e06f4230eb
I336718 <rutger_rauws@hotmail.com>
2016-01-11 14:30:42 +0100

    Updated the color picking using WPF standards

--- UNO/UnoClient/Game/CardHand.xaml.cs
+++ UNO/UnoClient/Game/CardHand.xaml.cs
@@ -45,106 +45,19 @@ namespace UnoClient.Game
 
             if(cardToBePlayed.Type == CardType.draw4Wild || cardToBePlayed.Type == CardType.wild)
             {
-                var window = new Window();
-                window.Closing += Window_Closing;
-                window.Width = 330;
-                window.Height = 100;
-                window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                ColorPickerWindow colorPicker = new ColorPickerWindow();
+                colorPicker.ShowDialog();
 
-                var MainstackPanel = new StackPanel { Orientation = Orientation.Vertical };
-                var ColorsStackPanel = new StackPanel { Orientation = Orientation.Horizontal };
-                var BtnStackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+                Brush selectedColor = colorPicker.selectedColor;
 
-                MainstackPanel.Name = "main";
-                ColorsStackPanel.Name = "color";
-                BtnStackPanel.Name = "btnn";
-
-                MainstackPanel.Width = 330;
-                MainstackPanel.Height = 100;
-                
-                ColorsStackPanel.Height = 30;
-                
-                BtnStackPanel.Height = 30;
-
-                MainstackPanel.HorizontalAlignment = HorizontalAlignment.Left;
-                MainstackPanel.VerticalAlignment = VerticalAlignment.Top;
-
-                RadioButton Yellow = new RadioButton();
-                Yellow.Content = "Yellow";
-                Yellow.Name = "yellowRbtn";
-                Yellow.IsChecked = true;
-                RadioButton Green = new RadioButton();
-                Green.Content = "Green";
-                Green.Name = "greenRbtn";
-                RadioButton Blue = new RadioButton();
-                Blue.Content = "Blue";
-                Blue.Name = "blueRbtn";
-                RadioButton Red = new RadioButton();
-                Red.Content = "Red";
-                Red.Name = "redRbtn";
-
-                Thickness radioBtnMargin = new Thickness(5,7,5,5);
-
-                Yellow.Margin = radioBtnMargin;
-                Green.Margin = radioBtnMargin;
-                Red.Margin = radioBtnMargin;
-                Blue.Margin = radioBtnMargin;
-
-                ColorsStackPanel.Children.Add(new Label { Content = "Choose a color" });
-                ColorsStackPanel.Children.Add(Yellow);
-                ColorsStackPanel.Children.Add(Green);
-                ColorsStackPanel.Children.Add(Blue);
-                ColorsStackPanel.Children.Add(Red);
-
-                Button ChooseBtn = new Button();
-                ChooseBtn.Content = "Choose";
-                radioBtnMargin = new Thickness(140, 0, 0, 0);
-                ChooseBtn.Margin = radioBtnMargin;
-                ChooseBtn.Click += ChooseBtn_Click;
-                BtnStackPanel.Children.Add(ChooseBtn);
-
-                MainstackPanel.Children.Add(ColorsStackPanel);
-                MainstackPanel.Children.Add(BtnStackPanel);
-
-                window.ApplyTemplate();
-                MainstackPanel.ApplyTemplate();
-                ColorsStackPanel.ApplyTemplate();
-                BtnStackPanel.ApplyTemplate();
-
-                window.Content = MainstackPanel;
-                window.ShowDialog();
-                try
-                {
-                    String Color = "";
-                    foreach (var item in ColorsStackPanel.Children)
-                    {
-                        if (item.GetType() == typeof(RadioButton))
-                            if (((RadioButton)item).IsChecked == true)
-                            {
-                                Color = ((RadioButton)item).Content.ToString();
-                            }
-                    }
-
-                    switch (Color)
-                    {
-                        case "Yellow":
-                            cardToBePlayed.Color = CardColor.Yellow;
-                            break;
-                        case "Green":
-                            cardToBePlayed.Color = CardColor.Green;
-                            break;
-                        case "Red":
-                            cardToBePlayed.Color = CardColor.Red;
-                            break;
-                        case "Blue":
-                            cardToBePlayed.Color = CardColor.Blue;
-                            break;
-                    }
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
+                if (selectedColor.Equals(Brushes.Red))
+                    cardToBePlayed.Color = CardColor.Red;
+                else if (selectedColor.Equals(Brushes.Green))
+                    cardToBePlayed.Color = CardColor.Green;
+                else if (selectedColor.Equals(Brushes.Blue))
+                    cardToBePlayed.Color = CardColor.Blue;
+                else if (selectedColor.Equals(Brushes.Yellow))
+                    cardToBePlayed.Color = CardColor.Yellow;
             }
 
             bool playSucces = parent.GameProxy.playCard(/*parent.GameID, */cardToBePlayed);//if special card, color chosen is saved in color attribute to be handled in the server
@@ -157,25 +70,6 @@ namespace UnoClient.Game
             else
                 MessageBox.Show("Invalid move");
             
-        }
-
-        private void ChooseBtn_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Window parent = (Window)((StackPanel)((StackPanel)((Button)sender).Parent).Parent).Parent;
-                parent.Closing -= Window_Closing;
-                parent.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            e.Cancel = true;
         }
 
         private void sv_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
---
+++ UNO/UnoClient/Game/ColorPickerWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UnoClient.Game
+{
+    /// <summary>
+    /// Interaction logic for ColorPickerWindow.xaml
+    /// </summary>
+    public partial class ColorPickerWindow : Window
+    {
+        public Brush selectedColor;
+        private bool picked = false;
+
+        public ColorPickerWindow()
+        {
+            selectedColor = Brushes.Red;
+
+            InitializeComponent();
+
+            foreach (Rectangle rect in colors.Children)
+                rect.MouseLeftButtonUp += Color_MouseLeftButtonUp;
+        }
+
+        private void Color_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            foreach (Rectangle rect in colors.Children)
+                rect.StrokeThickness = 1;
+
+            Rectangle color = (Rectangle)sender;
+            color.StrokeThickness = 5;
+
+            selectedColor = color.Fill;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            picked = true;
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(!picked) //If a person is trying to close the window without having pressed the 'Pick' button, the window won't close.
+                e.Cancel = true;
+        }
+    }
+}
